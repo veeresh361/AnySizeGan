d66fb0e3477532fcd9f8a89637d010a37fc9186b
Veeresh <kvveeresh591@gmail.com>
2023-04-27 17:11:08 +0530

    Added Extra Custom Layers

--- customlayers.py
+++ customlayers.py
@@ -118,7 +118,7 @@ class CustomResizingTwo(Layer):
         pass
 
     def call(self, x, mask=None):
-        newArray = tf.image.resize(x, [config.secondShapeOne, config.secondShapeTwo])#Dynamic resizing
+        newArray = tf.image.resize(x, [config.secondShapeOne, config.secondShapeTwo],preserve_aspect_ratio=True)#Dynamic resizing
         print(newArray.shape)
         return newArray
 
@@ -193,3 +193,90 @@ class ResidualLayerScal(Layer):
         x = self.add_op([x_path_1,x_shortcut])
         ##PATH 2
         return x
+    
+class ResidualLayerScalTwo(Layer):
+    
+    def __init__(self, f=None, s=None, fillter_size_top=None,
+               fillter_size_mid=None, fillter_size_bot=None):
+        
+        super(ResidualLayerScalTwo, self).__init__()
+        self.conv_top_1 = layers.Conv2D(128, (1, 1),
+                                                 strides=(1, 1), padding='valid')
+        # Make the hyperparameters different 
+        self.conv_top_2 =layers.Conv2D(64, (1, 1),strides=(1, 1), padding='valid')
+
+        self.conv_top_3=layers.Conv2D(64, (1, 1),strides=(1, 1), padding='valid')
+
+        self.batch_norm_top_1 = layers.BatchNormalization(axis=3)
+        self.batch_norm_top_2 = layers.BatchNormalization(axis=3)
+        self.batch_norm_top_3 = layers.BatchNormalization(axis=3)
+
+        self.activation_relu = layers.Activation('relu')
+        self.add_op = layers.Add()
+
+    def call(self, input_x, training=False):
+        x_shortcut = input_x
+
+        ##PATH 1
+        x_path_1 = input_x
+        # First CONV block of path 1
+        x_path_1 = self.conv_top_1(x_path_1)
+        x_path_1 = self.batch_norm_top_1(x_path_1, training=training)
+        x_path_1 = self.activation_relu(x_path_1)
+
+        # Second CONV block of path 1
+        x_path_1 = self.conv_top_2(x_path_1)
+        x_path_1 = self.batch_norm_top_2(x_path_1)
+        x_path_1 = self.activation_relu(x_path_1)
+
+        x_shortcut=self.conv_top_3(x_shortcut)
+        x_shortcut=self.batch_norm_top_3(x_shortcut)
+        
+        x = self.add_op([x_path_1,x_shortcut])
+        ##PATH 2
+        return x
+    
+
+class ResidualLayerScalThree(Layer):
+    
+    def __init__(self, f=None, s=None, fillter_size_top=None,
+               fillter_size_mid=None, fillter_size_bot=None):
+        
+        super(ResidualLayerScalThree, self).__init__()
+        self.conv_top_1 = layers.Conv2D(32, (1, 1),
+                                                 strides=(1, 1), padding='valid')
+        # Make the hyperparameters different 
+        self.conv_top_2 =layers.Conv2D(16, (1, 1),strides=(1, 1), padding='valid')
+
+        self.conv_top_3=layers.Conv2D(16, (1, 1),strides=(1, 1), padding='valid')
+
+        self.batch_norm_top_1 = layers.BatchNormalization(axis=3)
+        self.batch_norm_top_2 = layers.BatchNormalization(axis=3)
+        self.batch_norm_top_3 = layers.BatchNormalization(axis=3)
+
+        self.activation_relu = layers.Activation('relu')
+        self.add_op = layers.Add()
+
+    def call(self, input_x, training=False):
+        x_shortcut = input_x
+
+        ##PATH 1
+        x_path_1 = input_x
+        # First CONV block of path 1
+        x_path_1 = self.conv_top_1(x_path_1)
+        x_path_1 = self.batch_norm_top_1(x_path_1, training=training)
+        x_path_1 = self.activation_relu(x_path_1)
+
+        # Second CONV block of path 1
+        x_path_1 = self.conv_top_2(x_path_1)
+        x_path_1 = self.batch_norm_top_2(x_path_1)
+        x_path_1 = self.activation_relu(x_path_1)
+
+        x_shortcut=self.conv_top_3(x_shortcut)
+        x_shortcut=self.batch_norm_top_3(x_shortcut)
+        
+        x = self.add_op([x_path_1,x_shortcut])
+        ##PATH 2
+        return x
+    
+
